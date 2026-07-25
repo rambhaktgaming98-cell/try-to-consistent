663a7f17b26b768d87704fd1e0f5b0f60596ef55
rambhaktgaming98-cell <rambhaktgaming98@gmail.com>
2026-07-25 23:12:26 +0530

    Time: 1 ms (97.44%), Space: 42.7 MB (43.04%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,24 +1,22 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1) {
+        if (x < 2)
             return x;
-        }
 
         int left = 1;
-        int right = x;
+        int right = x / 2;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            long square = (long) mid * mid;
 
-            if (square == x) {
-                return mid;
-            } else if (square < x) {
+            if (mid <= x / mid) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
+
         return right;
+
     }
 }
