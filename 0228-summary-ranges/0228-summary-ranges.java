4936636023cca537914d23be054debee7454803b
rambhaktgaming98-cell <rambhaktgaming98@gmail.com>
2026-08-13 00:01:58 +0530

    Time: 3 ms (78.51%), Space: 43.3 MB (20.75%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,28 @@
+ import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int start = nums[i];
+
+            while (i + 1 < n && nums[i + 1] == nums[i] + 1) {
+                i++;
+            }
+
+            if (start == nums[i]) {
+                result.add(String.valueOf(start));
+            } else {
+                result.add(start + "->" + nums[i]);
+            }
+        }
+
+        return result;
+    }
+}
